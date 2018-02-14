11d49fb40e5bbed45af8a1e880cb789f5a868ea6
EvgeniyPankov <pankov2003@mail.ru>
2018-02-15 01:52:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ class BotHandler:
 token = "500408452:AAEE6jBGtMV5z7gntDxHUB5mdFOWjuE_sxw"
 greet_bot = BotHandler(token)  
 greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-clanone = ('Evgeniy_Pankov')
+clanone = 'Evgeniy_Pankov'
 now = datetime.datetime.now()
 
 def main():  
@@ -56,7 +56,7 @@ def main():
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['username']
         
-       if last_chat_name() in clanone:
+     if last_chat_name ==  clanone :
 		greet_bot.send_message(last_chat_id, 'Доброе утро, жрец {}'.format(last_chat_name))
 		
         new_offset = last_update_id + 1
